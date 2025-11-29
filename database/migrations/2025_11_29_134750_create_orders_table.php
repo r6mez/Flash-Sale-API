a94ca28d29ef9c39dd6d43593ce7e440f618690a
Ramez Medhat <iramezdev@gmail.com>
2025-11-29 16:09:41 +0200

    Created migrations and models

---
+++ database/migrations/2025_11_29_134750_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Hold;
+use App\Models\Product;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Hold::class)->nullable();
+            $table->foreignIdFor(Product::class);
+            $table->unsignedInteger("qty");
+            $table->unsignedInteger("amount_cents");
+            $table->enum("status", ["pending", "paid", "cancelled"]);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
