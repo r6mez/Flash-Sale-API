718147e688fa2bbc86c3098010e6caeba1be47a8
Ramez Medhat <iramezdev@gmail.com>
2026-01-06 20:38:09 +0200

    Fix: Changing default cache to redis and increasing TTL

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Cache;
 class ProductController extends Controller
 {
     function show(int $id){
-        return Cache::remember("product:{$id}", 60, function () use ($id) {
+        return Cache::remember("product:{$id}", 3600, function () use ($id) {
             return Product::findOr($id, function () {
                 abort(404, 'Product not found');
             });
