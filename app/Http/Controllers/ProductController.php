957254ffbcb3ec716029ca7158c2e6e053ff0513
Ramez Medhat <iramezdev@gmail.com>
2026-01-06 21:11:49 +0200

    Fix: Get request caching 404 response

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,16 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 
 class ProductController extends Controller
 {
-    function show(int $id){
-        return Cache::remember("product:{$id}", 3600, function () use ($id) {
-            return Product::findOr($id, function () {
-                abort(404, 'Product not found');
-            });
+    function show(int $id)
+    {
+        $product = Cache::remember("product:{$id}", 3600, function () use ($id) {
+            return Product::find($id);
         });
+
+        if (!$product) {
+            return response()->json(['message' => 'Product Not found'], 404);
+        }
+
+        return $product;
     }
 }
